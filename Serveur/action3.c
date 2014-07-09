e740e5ddb9ccff47c593ebe9f87d4db100f7a802
boulan_d <boulan_d@titaneman-virtual-machine>
2014-07-10 01:19:54 +0200

    I put generic message in functions to implement

--- Serveur/action3.c
+++ Serveur/action3.c
@@ -2,10 +2,12 @@
 
 t_serveur	*cmdfork(t_serveur *s, int fd)
 {
+  printf("pfk #n\n");
 }
 
 t_serveur	*cmdslot(t_serveur *s, int fd)
 {
+  printf("Slot: ?");
 }
 
 t_serveur	*cmddie(t_serveur *s, int fd)
